f045c8dbd283745bd8c7bab3c25297bebf4a0bfb
GUOKKK <528032175@qq.com>
2024-03-18 18:03:15 +0800

    init

---
+++ src/components/AppNavigator.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        dasdas
+    </div>
+</template>
+
+<script lang="ts" setup>
+import Vue from 'vue'
+
+
+</script>
+
+<style scoped></style>
---
+++ src/views/AppView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import AppNavigator from '@/components/AppNavigator.vue'
+</script>
+<template>
+  <div class="app-wrapper">
+    <header>
+      <AppNavigator />
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+
